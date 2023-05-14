ad78f962954d66b7e99ce33d65f2518baacfc48d
Yevhenii2022 <3starosta3@gmail.com>
2023-05-14 17:23:47 +0300

    add

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+// import { nanoid } from 'nanoid';
+// import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import { Text } from 'components';
 
 export class Todos extends Component {
   render() {
